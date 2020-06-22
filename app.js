b5737e833e217695a162325b7b4b7acc1ac64937
DanielMin2009 <daniel.minguella.blasco@gmail.com>
2020-06-22 13:50:44 +0200

    review syntax classes

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 "use strict";
 (() => {
+    // class Avenger {
+    //     name: string;
+    //     team: string;
+    //     realName?: string;
+    //     canFight: boolean;
+    //     winFights: number;
+    //     constructor( name: string, team: string) {
+    //         this.name = name;
+    //         this.team = team;
+    //     }
+    // }
+    // let antman: Avenger = new Avenger('Antman', 'Captain');
+    // console.log(antman);
+    class Avenger {
+        constructor(name, team, realName, canFight = true, winFights = 0) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+            this.canFight = canFight;
+            this.winFights = winFights;
+        }
+    }
+    let antman = new Avenger("Antman", "Captain");
+    console.log(antman);
 })();
--- app.ts
+++ app.ts
@@ -1,4 +1,33 @@
 (() => {
+  // class Avenger {
+  //     name: string;
+  //     team: string;
+  //     realName?: string;
 
+  //     canFight: boolean;
+  //     winFights: number;
 
+  //     constructor( name: string, team: string) {
+  //         this.name = name;
+  //         this.team = team;
+  //     }
+  // }
+
+  // let antman: Avenger = new Avenger('Antman', 'Captain');
+
+  // console.log(antman);
+
+  class Avenger {
+    constructor(
+      public name: string,
+      public team: string,
+      public realName?: string,
+      public canFight: boolean = true,
+      public winFights: number = 0
+    ) {}
+  }
+
+  let antman: Avenger = new Avenger("Antman", "Captain");
+
+  console.log(antman);
 })();
